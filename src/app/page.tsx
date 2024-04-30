1e4d06838c28ac8fcf5e652213c998e44e06c929
joshtin2505 <justincastro2505@gmail.com>
2024-04-30 16:11:58 -0500

    Add Hero component to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import KeyboardManager from "@/components/KeyboardManager"
+import Hero from "@/components/sections/Hero"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Hero />
       <KeyboardManager />
     </main>
   )
