e022b5dc5fc3c4bd7d996f8bdc507618d5eddc98
joshtin2505 <justincastro2505@gmail.com>
2024-05-04 22:54:14 -0500

    refactor: update import statement for Hero component in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import KeyboardManager from "@/components/KeyboardManager"
-import HomeHero from "@/components/home/HomeHero"
+import Hero from "@/components/curriculum/sections/Hero"
 
 export default function Home() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <HomeHero />
+        <Hero />
       </main>
       <KeyboardManager />
     </>
