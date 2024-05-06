1254b6314c0dff21c12112c9130bd6ef6de79f2b
joshtin2505 <justincastro2505@gmail.com>
2024-05-05 21:13:07 -0500

    refactor: Update NavBar component styles and add search functionality

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -1,26 +1,65 @@
+"use client"
 import Link from "next/link"
 import "./NavBar.css"
+import { useEffect, useState } from "react"
+import "./NavBar.css"
+
 function NavBar() {
+  const [os, setOs] = useState("")
+
+  useEffect(() => {
+    const userAgent = window.navigator.userAgent
+    if (userAgent.includes("Win")) {
+      setOs("Windows")
+    } else if (userAgent.includes("Mac")) {
+      setOs("MacOS")
+    } else if (userAgent.includes("Linux")) {
+      setOs("Linux")
+    } else if (userAgent.includes("Android")) {
+      setOs("Android")
+    } else if (userAgent.includes("iOS")) {
+      setOs("iOS")
+    } else {
+      setOs("Unknown")
+    }
+  }, [])
+
   return (
-    <header className="absolute top-0 w-full h-auto print:hidden">
+    <header className=" top-0 w-full h-auto print:hidden">
       <nav className="flex items-center px-8 justify-between border-b dark:border-b-white/30 ">
-        <Link href="/#hero" className="logo ">
+        <Link href="/#hero" className="logo cursor-pointer">
           <h3 className="text-2xl font-mono hover:underline underline-offset-4">
             Joshtin.Dev
           </h3>
         </Link>
         <ul className="flex justify-between items-center p-4 w-auto gap-3">
           <li>
+            <div
+              className="flex w-48 justify-between bg-neutral-950 rounded pl-2 py-1"
+              onClick={() => console.log("Search")}
+            >
+              <span className="text-neutral-500">Serch...</span>
+              {os === "MacOS" ? (
+                <kbd className="bg-neutral-500/30 text-neutral-400/90 rounded-md px-1 scale-95">
+                  Cmd + K
+                </kbd>
+              ) : (
+                <kbd className="bg-neutral-500/30 text-neutral-400/90 rounded-md px-1 scale-75">
+                  Ctrl + K
+                </kbd>
+              )}
+            </div>
+          </li>
+          <li>
             <Link href="/curriculum">Curriculum</Link>
           </li>
-          <li>
-            <Link href="#about">About</Link>
-          </li>
-          <li>
-            <Link href="#projects">Projects</Link>
-          </li>
-          <li>
-            <Link href="#contact">Contact</Link>
+          <li className="text-neutral-500  text-center">
+            <Link className="flex gap-1" href="/blog">
+              Blog
+              <span className="border border-neutral-400 bg-neutral-500/10 rounded-full px-2 text-sm text-center">
+                comming
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
