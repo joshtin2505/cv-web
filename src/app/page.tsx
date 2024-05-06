ed808be76aeb80a359d49a887e0dc4e781b39b84
joshtin2505 <justincastro2505@gmail.com>
2024-05-05 21:12:08 -0500

    refactor: Update NavBar component styles and add search functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import KeyboardManager from "@/components/KeyboardManager"
 import Hero from "@/components/curriculum/sections/Hero"
-import NavBar from "@/components/nav/NavBar"
 
 export default function Home() {
   return (
     <>
-      <NavBar />
       <div className="">
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
           <Hero />
