78625e282eef900cd3d9342e911cf3743ae0ebd2
joshtin2505 <justincastro2505@gmail.com>
2024-05-05 20:19:49 -0500

    refactor: Add NavBar component to page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import KeyboardManager from "@/components/KeyboardManager"
 import Hero from "@/components/curriculum/sections/Hero"
+import NavBar from "@/components/nav/NavBar"
 
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Hero />
-      </main>
+      <NavBar />
+      <div className="">
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <Hero />
+        </main>
+        <aside></aside>
+      </div>
       <KeyboardManager />
     </>
   )
