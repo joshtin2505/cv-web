ad6b11ee7cb5b98de2e89da3a7cee7a5fb8e5982
joshtin2505 <justincastro2505@gmail.com>
2024-05-05 20:20:06 -0500

    refactor: Update Section component to include optional id prop

--- src/components/curriculum/Section.tsx
+++ src/components/curriculum/Section.tsx
@@ -1,17 +1,23 @@
 import { type ReactNode } from "react"
 
-function Section({ title, children }: { title?: string; children: ReactNode }) {
+function Section({
+  title,
+  children,
+  id = "",
+}: {
+  title?: string
+  children: ReactNode
+  id?: string
+}) {
   return (
-    <div>
-      <section className="max-w-3xl mx-auto mb-6 relative">
-        {title && (
-          <h2 className="text-2xl mb-2 font-bold leading-normal dark:text-white">
-            {title}
-          </h2>
-        )}
-        {children}
-      </section>
-    </div>
+    <section id={id} className="max-w-3xl mx-auto mb-6 relative">
+      {title && (
+        <h2 className="text-2xl mb-2 font-bold leading-normal dark:text-white">
+          {title}
+        </h2>
+      )}
+      {children}
+    </section>
   )
 }
 
