9206ed779c1ec1ee430344f72f7b2003d7607669
joshtin2505 <justincastro2505@gmail.com>
2024-05-07 16:56:00 -0500

    refactor: Update Projects section styles for responsive layout

--- src/components/curriculum/sections/Projects.tsx
+++ src/components/curriculum/sections/Projects.tsx
@@ -4,26 +4,34 @@ import { projects } from "@/mocks/cv.json"
 function Projects() {
   return (
     <Section title="Proyectos">
-      <ul className="grid grid-template-colums-auto-fill gap-1">
+      <ul className="grid grid-template-colums-auto-fill gap-2">
         {projects.map(({ description, highlights, name, url, isActive }) => (
           <li key={name}>
             <article className="flex flex-col p-4 rounded-md border dark:border-neutral-600 gap-4 h-full">
               <header className="flex-1">
                 <h3 className="mb-2 flex gap-1">
                   <Link
-                    className="text-neutral-100 hover:underline hover:text-neutral-300"
+                    className="dark:text-neutral-100 hover:underline dark:hover:text-neutral-300"
                     href={url}
+                    target="_blank"
                     title={`Ver el proyecto ${name}`}
                   >
                     {name}
                   </Link>
                   {isActive && <span className="scale-150"> &#8226; </span>}
                 </h3>
-                <p>{description}</p>
+                <p className="text-sm">{description}</p>
               </header>
-              <footer>
+              <footer className="flex flex-wrap justify-start items-center gap-1">
                 {highlights.map((highlight) => {
-                  return <span key={highlight}>{highlight}</span>
+                  return (
+                    <span
+                      className="rounded-lg dark:bg-neutral-800 text-sm font-medium px-2 "
+                      key={highlight}
+                    >
+                      {highlight}
+                    </span>
+                  )
                 })}
               </footer>
             </article>
