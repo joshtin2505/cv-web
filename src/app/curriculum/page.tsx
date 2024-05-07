7e8ac69667d402ac36bd52a07284fe9cb9d9f49a
joshtin2505 <justincastro2505@gmail.com>
2024-05-07 16:56:59 -0500

    feat: Add Education section to curriculum page

--- src/app/curriculum/page.tsx
+++ src/app/curriculum/page.tsx
@@ -1,5 +1,6 @@
 import KeyboardManager from "@/components/KeyboardManager"
 import About from "@/components/curriculum/sections/About"
+import Education from "@/components/curriculum/sections/Education"
 import Experience from "@/components/curriculum/sections/Experience"
 import Hero from "@/components/curriculum/sections/Hero"
 import Projects from "@/components/curriculum/sections/Projects"
@@ -8,10 +9,11 @@ import Skills from "@/components/curriculum/sections/Skills"
 function page() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 relative">
+      <main className=" min-h-screen p-24 relative">
         <Hero />
         <About />
         <Experience />
+        <Education />
         <Projects />
         <Skills />
       </main>
