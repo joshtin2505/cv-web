9fbf9915de3977489ec00a05695580f55821c5e7
joshtin2505 <justincastro2505@gmail.com>
2024-05-06 20:28:34 -0500

    refactor: Add curriculum sections to page layout

--- src/app/curriculum/page.tsx
+++ src/app/curriculum/page.tsx
@@ -1,11 +1,19 @@
 import KeyboardManager from "@/components/KeyboardManager"
+import About from "@/components/curriculum/sections/About"
+import Experience from "@/components/curriculum/sections/Experience"
 import Hero from "@/components/curriculum/sections/Hero"
+import Projects from "@/components/curriculum/sections/Projects"
+import Skills from "@/components/curriculum/sections/Skills"
 
 function page() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24 relative">
         <Hero />
+        <About />
+        <Experience />
+        <Projects />
+        <Skills />
       </main>
       <KeyboardManager />
     </>
