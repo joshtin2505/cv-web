0021b742a26d441538cadd6d4f8a386515f05f51
joshtin2505 <justincastro2505@gmail.com>
2024-05-06 20:28:22 -0500

    refactor: Update tailwind.config.ts with responsive screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -14,7 +14,37 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    screens: {
+      "max-2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      "max-xl": { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      "max-lg": { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      "max-md": { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      "max-sm": { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: "1279px",
+      // => @media (min-width: 1279px) { ... }
+
+      lg: "1023px",
+      // => @media (min-width: 1023px) { ... }
+
+      md: "767px",
+      // => @media (min-width: 767px) { ... }
+
+      sm: "639px",
+      // => @media (min-width: 639px) { ... }
+    },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
