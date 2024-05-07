24d6dbad658a26c9d937f7a8e73011508c4a25d0
joshtin2505 <justincastro2505@gmail.com>
2024-05-07 16:58:07 -0500

    refactor: Simplify main container styles in Section component

--- src/components/curriculum/Section.tsx
+++ src/components/curriculum/Section.tsx
@@ -10,10 +10,7 @@ function Section({
   id?: string
 }) {
   return (
-    <section
-      id={id}
-      className=" lg:w-[700px] max-lg:max-w-[700px] mx-auto mb-8 relative"
-    >
+    <section id={id} className="max-w-[700px] mx-auto mb-8 relative">
       {title && (
         <h2 className="text-2xl mb-2 font-bold leading-normal dark:text-white">
           {title}
