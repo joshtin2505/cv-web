3a7a049c519c72ca676b67603e6364dbe96c4737
joshtin2505 <justincastro2505@gmail.com>
2024-05-06 21:44:12 -0500

    refactor: Update Section component styles for responsive layout

--- src/components/curriculum/Section.tsx
+++ src/components/curriculum/Section.tsx
@@ -10,7 +10,10 @@ function Section({
   id?: string
 }) {
   return (
-    <section id={id} className="max-w-3xl mx-auto mb-6 relative">
+    <section
+      id={id}
+      className=" lg:w-[700px] max-lg:max-w-[700px] mx-auto mb-8 relative"
+    >
       {title && (
         <h2 className="text-2xl mb-2 font-bold leading-normal dark:text-white">
           {title}
