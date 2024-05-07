baf662bf93f4acdc4c91294b5e60393373301026
joshtin2505 <justincastro2505@gmail.com>
2024-05-07 16:59:50 -0500

    refactor: Update Education section styles for better readability and consistency

--- src/components/curriculum/sections/Education.tsx
+++ src/components/curriculum/sections/Education.tsx
@@ -3,22 +3,25 @@ import { education } from "@/mocks/cv.json"
 function Education() {
   return (
     <Section title="Educación">
-      <ul>
+      <ul className="flex flex-col gap-8">
         {education.map(({ institution, startDate, endDate, area }, i) => {
           const startYear = new Date(startDate).getFullYear()
           const endYear =
             endDate != null ? new Date(endDate).getFullYear() : "Actual"
           const years = `${startYear} - ${endYear}`
-
           return (
             <li key={i}>
               <article>
-                <header>
+                <header className="flex justify-between items-start">
                   <div>
-                    <h3>{institution}</h3>
+                    <h3 className="font-medium dark:text-neutral-300">
+                      {institution}
+                    </h3>
                   </div>
 
-                  <time>{years}</time>
+                  <time className="dark:text-neutral-400 text-sm min-w-24 max-md:text-right">
+                    {years}
+                  </time>
                 </header>
 
                 <footer>
