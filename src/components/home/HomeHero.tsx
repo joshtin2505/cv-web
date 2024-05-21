497d90d37bdcb37261401caede34a5e7bf948e48
justincastro2505 <justincastro2505@gmail.com>
2024-05-21 22:24:12 +0000

    Update Hero Commponent

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image"
 import React from "react"
-import Logo from "../Logo"
 import { basics } from "@/mocks/cv.json"
+import "./HomeHero.css"
+
 function HomeHero() {
   return (
-    <section className="w-full">
+    <section className="w-full p-x-20 flex items-center justify-around">
+      <div className="flex flex-col items-start justify-center gap-3 w-[400px]">
+        <h1 className="text-6xl">Hey Here!</h1>
+        <h2 className="text-5xl font-semibold font-mono name ">
+          I'm
+          <span className="rainbow"> Joshtin.dev</span>
+        </h2>
+      </div>
       <div className="">
-        <Logo />
         <Image
           className="rounded-full"
           src={basics.image}
