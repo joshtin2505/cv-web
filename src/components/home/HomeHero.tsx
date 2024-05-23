71465438cb64d6073f3974f23410bff03e71667a
joshtin2505 <justincastro2505@gmail.com>
2024-05-22 22:18:36 -0500

    refactor: Update HomeHero component layout and styles

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -5,23 +5,30 @@ import "./HomeHero.css"
 
 function HomeHero() {
   return (
-    <section className="w-full p-x-20 flex items-center justify-around">
-      <div className="flex flex-col items-start justify-center gap-3 w-[400px]">
-        <h1 className="text-6xl">Hey Here!</h1>
-        <h2 className="text-5xl font-semibold font-mono name ">
-          I'm
-          <span className="rainbow"> Joshtin.dev</span>
-        </h2>
-      </div>
-      <div className="">
-        <Image
-          className="rounded-full"
-          src={basics.image}
-          alt="johstn.dev"
-          width={168}
-          height={168}
-        />
-      </div>
+    <section className="w-full px-28 h-[90vh] mt-[10vh]">
+      <header className="flex items-center justify-between">
+        <div className="flex flex-col items-start justify-center gap-3 w-[400px]">
+          <h1 className="text-6xl ">Hey Here!</h1>
+          <h2 className="text-5xl font-semibold font-mono name ">
+            I&apos;m
+            <span className="rainbow"> Joshtin.dev</span>
+          </h2>
+        </div>
+        <div className="">
+          <Image
+            className="rounded-md aspect-square w-40"
+            src={basics.image}
+            alt="johstn.dev"
+            width={1000}
+            height={1000}
+          />
+        </div>
+      </header>
+      <footer className="w-full z-0">
+        <h4 className="text-xl font-normal font-mono tracking-wide opacity-50 ">
+          {basics.label}
+        </h4>
+      </footer>
     </section>
   )
 }
