3857cc44d4f4500bec684926f5a15ff1c43ac4ba
joshtin2505 <justincastro2505@gmail.com>
2024-06-12 17:39:01 -0500

    refactor: Improve HomeHero component layout and add social media icons

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -2,33 +2,63 @@ import Image from "next/image"
 import React from "react"
 import { basics } from "@/mocks/cv.json"
 import "./HomeHero.css"
+import { FaEnvelope, FaGithub, FaLinkedinIn, FaNpm } from "react-icons/fa6"
 
 function HomeHero() {
   return (
-    <section className="w-full px-28 h-[90vh] mt-[10vh]">
-      <header className="flex items-center justify-between">
-        <div className="flex flex-col items-start justify-center gap-3 w-[400px]">
-          <h1 className="text-6xl ">Hey Here!</h1>
-          <h2 className="text-5xl font-semibold font-mono name ">
-            I&apos;m
-            <span className="rainbow"> Joshtin.dev</span>
-          </h2>
-        </div>
-        <div className="">
-          <Image
-            className="rounded-md aspect-square w-40"
-            src={basics.image}
-            alt="johstn.dev"
-            width={1000}
-            height={1000}
-          />
-        </div>
-      </header>
-      <footer className="w-full z-0">
-        <h4 className="text-xl font-normal font-mono tracking-wide opacity-50 ">
-          {basics.label}
-        </h4>
-      </footer>
+    <section
+      style={{ minHeight: "calc(100dvh - 69px)" }}
+      className="w-full max-w-5xl mx-auto lg:px-0 px-5 py-8"
+    >
+      <div className="md:block flex flex-col items-center">
+        <header className="flex items-center md:justify-between justify-center">
+          <div className="flex flex-col items-start justify-center gap-3 w-[400px]">
+            <h1 className="text-6xl font-mono tracking-tighter ">Hey Here!</h1>
+            <h2 className="text-5xl font-semibold font-mono name ">
+              I&apos;m
+              <span className="rainbow"> Joshtin.dev</span>
+            </h2>
+          </div>
+          <figure className="h-full flex justify-center">
+            <Image
+              className="rounded-md aspect-square w-44 object-cover hidden md:block"
+              src={basics.image}
+              alt="johstn.dev"
+              width={1000}
+              height={1000}
+              loading="eager"
+            />
+          </figure>
+        </header>
+        <footer className="md:w-full z-0 flex md:justify-between justify-center items-center mt-1 flex-col md:flex-row">
+          <h4 className="md:text-xl text-lg font-normal font-mono tracking-wide opacity-50 text-pretty">
+            {basics.label}
+          </h4>
+          {/* sI CAMBIAS EL  ORDEN DEL JSON PUEDES CAUSAR UN ERROR EN EL DIRECCIONAMIENTTO, CORRIGE ESO */}
+          <ul className=" flex w-44 md:mt-0 mt-2 md:opacity-100 opacity-80 justify-around">
+            <li>
+              <a href={`mailto:${basics.email}`} className=" ">
+                <FaEnvelope className="h-7 w-7 transition-all hover:boder-2 dark:hover:text-white/70 dark:text-white dark:bg-neutral-800/50 dark:hover:bg-neutral-500/20 p-1 rounded dark:border-neutral-800 border" />
+              </a>
+            </li>
+            <li>
+              <a href={`{basics.profiles[0]}`} className=" ">
+                <FaLinkedinIn className="h-7 w-7 transition-all hover:boder-2 dark:hover:text-white/70 dark:text-white dark:bg-neutral-800/50 dark:hover:bg-neutral-500/20 p-1 rounded dark:border-neutral-800 border" />
+              </a>
+            </li>
+            <li>
+              <a href={`{basics.profiles[2]}`} className=" ">
+                <FaGithub className="h-7 w-7 transition-all hover:boder-2 dark:hover:text-white/70 dark:text-white dark:bg-neutral-800/50 dark:hover:bg-neutral-500/20 p-1 rounded dark:border-neutral-800 border" />
+              </a>
+            </li>
+            <li>
+              <a href={`{basics.profiles[3]}`} className=" ">
+                <FaNpm className="h-7 w-7 transition-all hover:boder-2 dark:hover:text-white/70 dark:text-white dark:bg-neutral-800/50 dark:hover:bg-neutral-500/20 p-1 rounded dark:border-neutral-800 border" />
+              </a>
+            </li>
+          </ul>
+        </footer>
+      </div>
     </section>
   )
 }
