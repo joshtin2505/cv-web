72c6b2fad0fbb98bc13eebc5d6cc2b193ac1f2fa
joshtin2505 <justincastro2505@gmail.com>
2024-06-14 14:06:35 -0500

    refactor: Add support for HTTPS in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "bcfiles.vercel.app",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 }
