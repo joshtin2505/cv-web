f80efb7d83e52eef70bd14bff9bee40f22ae0a2d
joshtin2505 <justincastro2505@gmail.com>
2024-06-14 01:17:34 -0500

    Fix Typing bug and Add Responsive Css

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -1,83 +1,89 @@
 import Image from "next/image"
-import React from "react"
 import cvJson from "@/mocks/cv.json"
 import "./HomeHero.css"
 import { FaEnvelope, FaGithub, FaLinkedinIn, FaNpm } from "react-icons/fa6"
+import {} from "react-icons"
 import Link from "next/link"
 
 function HomeHero() {
   const { basics } = cvJson
+  const SOCIAL_ICONS: Record<
+    string,
+    typeof FaLinkedinIn | typeof FaGithub | typeof FaNpm
+  > = {
+    LinkedIn: FaLinkedinIn,
+    GitHub: FaGithub,
+    NPM: FaNpm,
+  }
   return (
     <section
       style={{ minHeight: "calc(100dvh - 69px)" }}
-      className="w-full max-w-5xl mx-auto lg:px-0 px-5 py-8"
+      className="w-full dark:bg-[url('/BgDarkHero.png')] bg-[url('/BgLightHero.png')] bg-cover bg-center bg-no-repeat relative before:absolute before:inset-0 before:bg-gradient-to-b before:from-transparent dark:before:to-black before:to-white "
     >
-      <div className="md:block flex flex-col items-center">
-        <header className="flex items-center md:justify-between justify-center">
-          <div className="flex flex-col items-start justify-center gap-3 w-[400px]">
-            <h1 className="text-6xl font-mono tracking-tighter ">Hey Here!</h1>
-            <h2 className="text-5xl font-semibold font-mono name ">
-              I&apos;m
-              <span className="rainbow"> Joshtin.dev</span>
-            </h2>
-          </div>
-          <figure className="h-full flex justify-center">
-            <Image
-              className="rounded-md aspect-square w-44 object-cover hidden md:block"
-              src={basics.image}
-              alt="johstn.dev"
-              width={1000}
-              height={1000}
-              loading="eager"
-            />
-          </figure>
-        </header>
-        <footer className="md:w-full z-0 flex md:justify-between justify-center items-center mt-1 flex-col md:flex-row">
-          <h4 className="md:text-xl text-lg font-normal font-mono tracking-wide opacity-50 text-pretty">
-            {basics.label}
-          </h4>
-          {/* sI CAMBIAS EL  ORDEN DEL JSON PUEDES CAUSAR UN ERROR EN EL DIRECCIONAMIENTTO, CORRIGE ESO */}
-          <ul className=" flex w-44 md:mt-0 mt-2 md:opacity-100 opacity-80 justify-around">
-            <li>
-              <a
-                href={`mailto:${basics.email}`}
-                className=" h-7 w-7 transition-all hover:boder-2 dark:hover:text-white/70 dark:text-white dark:bg-neutral-800/50 dark:hover:bg-neutral-500/20 p-1 rounded dark:border-neutral-800 border"
-              >
-                <FaEnvelope size={24} />
-              </a>
-            </li>
-            <li>
-              <Link
-                href={`${basics.profiles[0].url}`}
-                rel="noreferrer"
-                target="_blank"
-                className=" h-7 w-7 transition-all hover:boder-2 dark:hover:text-white/70 dark:text-white dark:bg-neutral-800/50 dark:hover:bg-neutral-500/20 p-1 rounded dark:border-neutral-800 border"
-              >
-                <FaLinkedinIn size={24} />
-              </Link>
-            </li>
-            <li>
-              <Link
-                href={`${basics.profiles[2].url}`}
-                rel="noreferrer"
-                target="_blank"
-                className=" h-7 w-7 transition-all hover:boder-2 dark:hover:text-white/70 dark:text-white dark:bg-neutral-800/50 dark:hover:bg-neutral-500/20 p-1 rounded dark:border-neutral-800 border"
-              >
-                <FaGithub size={24} />
-              </Link>
-            </li>
-            <li>
-              <Link
-                href={`${basics.profiles[3].url}`}
-                rel="noreferrer"
-                target="_blank"
-                className=" h-7 w-7 transition-all hover:boder-2 dark:hover:text-white/70 dark:text-white dark:bg-neutral-800/50 dark:hover:bg-neutral-500/20 p-1 rounded dark:border-neutral-800 border"
-              >
-                <FaNpm size={24} />
-              </Link>
-            </li>
-          </ul>
-        </footer>
+      <div className="max-w-5xl mx-auto lg:px-0 sm:px-5 py-8 relative z-10">
+        <div className="md:block flex flex-col scale-75 sm:scale-100 ">
+          {/* <div className="flex items-start flex-col"> */}
+          <header className="flex items-center md:justify-between justify-center">
+            <div className="flex flex-col items-start justify-center gap-3 w-[400px]">
+              <h1 className=" text-6xl font-mono tracking-tighter ">
+                Hey Here!
+              </h1>
+              <h2 className="text-5xl font-semibold font-mono name ">
+                I&apos;m&nbsp;
+                <span className="rainbow text-5xl font-semibold font-mono">
+                  Joshtin.dev
+                </span>
+              </h2>
+            </div>
+            <figure className="h-full flex justify-center">
+              <Image
+                className="rounded-md aspect-square w-44 object-cover hidden md:block"
+                src={basics.image}
+                alt="johstn.dev"
+                width={1000}
+                height={1000}
+                loading="eager"
+              />
+            </figure>
+          </header>
+          <footer className="md:w-full z-0 flex md:justify-between justify-center items-center mt-1 flex-col md:flex-row">
+            <h4 className="md:text-xl tracking-normal text-lg font-medium font-mono xs:tracking-wide opacity-50 text-nowrap ">
+              {basics.label}
+            </h4>
+            {/* sI CAMBIAS EL  ORDEN DEL JSON PUEDES CAUSAR UN ERROR EN EL DIRECCIONAMIENTTO, CORRIGE ESO */}
+            <ul className=" flex w-44 md:mt-0 mt-2 md:opacity-100 opacity-80 justify-around">
+              <li className="h-7 w-7 transition-all hover:boder-2 dark:hover:text-white/70 dark:text-white dark:bg-neutral-800/50 dark:hover:bg-neutral-500/20 rounded dark:border-neutral-800 border">
+                <Link
+                  className="w-full h-full grid place-items-center"
+                  href={`mailto:${basics.email}`}
+                >
+                  <FaEnvelope size={18} />
+                </Link>
+              </li>
+              {basics.profiles.map(({ network, url }, i) => {
+                if (!SOCIAL_ICONS[network]) return null
+                if (i === 1) return null
+                const Icon = SOCIAL_ICONS[network]
+                return (
+                  <li
+                    key={network}
+                    className="h-7 w-7 transition-all hover:boder-2 dark:hover:text-white/70 dark:text-white dark:bg-neutral-800/50 dark:hover:bg-neutral-500/20 rounded dark:border-neutral-800 border"
+                  >
+                    <Link
+                      className="w-full h-full grid place-items-center"
+                      href={url}
+                      rel="noreferrer"
+                      target="_blank"
+                    >
+                      <Icon size={20} />
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </footer>
+          {/* </div> */}
+        </div>
       </div>
     </section>
   )
