decec37812dbe94ddb329ed74b784f5a1aa26fad
joshtin2505 <justincastro2505@gmail.com>
2024-06-14 16:11:40 -0500

    refactor: Update import path for ProjectsPortfolio component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import KeyboardManager from "@/components/KeyboardManager"
 import Basic from "@/components/Layouts/BasicLayout"
 import HomeHero from "@/components/home/HomeHero"
 import ToolsIUse from "@/components/home/ToolsIUse"
-import ProjectsPortfolio from "@/components/portfolio/Project"
+import ProjectsPortfolio from "@/components/home/Project"
 
 export default function Home() {
   return (
