3aa3922222a979890a47bc3885c6cd3d0e7823dd
joshtin2505 <justincastro2505@gmail.com>
2024-06-14 16:15:45 -0500

    refactor: Improve CardProject component layout and styles
    This commit refactors the CardProject component to improve its layout and styles. It updates the CSS classes to enhance the visual presentation of the project card, including the flexbox alignment, image aspect ratio, and text positioning. These changes enhance the user experience and align with recent user commits to improve the application's functionality and responsiveness.

--- src/components/ui/CardProject.tsx
+++ src/components/ui/CardProject.tsx
@@ -4,10 +4,10 @@ import Image from "next/image"
 
 function CardProject({ project }: { project: Projects }) {
   return (
-    <div className=" bg-neutral-300/40 dark:bg-neutral-800/40 rounded backdrop-blur-md max-w-[90%] flex shadow-md transition-shadow hover:shadow-xl border border-black/50 dark:border-white/50 rounded-b">
+    <div className=" bg-neutral-300/40 dark:bg-neutral-800/40 rounded backdrop-blur-md max-w-[90%] flex flex-col md:flex-row shadow-md transition-shadow hover:shadow-xl border border-black/50 dark:border-white/50 rounded-b">
       <figure className="aspect-video">
         <Image
-          className="h-full object-cover aspect-video rounded-t"
+          className="h-full object-cover aspect-video md:rounded-l rounded-t"
           src={project.img[0]}
           alt={project.name}
           width={1000}
@@ -18,6 +18,8 @@ function CardProject({ project }: { project: Projects }) {
         <Link
           className="text-xl hover:underline capitalize font-medium dark:text-neutral-50"
           href={project.url}
+          target="_blank"
+          rel="noreferrer"
         >
           {project.name}
           <strong
@@ -47,9 +49,9 @@ function CardProject({ project }: { project: Projects }) {
               </span>
             ))}
           </div>
-          <div className="flex items-center text-center gap-2 px-2">
+          <div className="flex items-center  gap-2 px-2">
             <hr className="w-full border-t-black/50 dark:border-t-white/50" />
-            <h5 className="text-nowrap text-neutral-600 dark:text-neutral-300">
+            <h5 className="text-nowrap text-neutral-600 dark:text-neutral-300 text-center">
               Tech Stack
             </h5>
             <hr className="w-full border-t-black/50 dark:border-t-white/50" />
